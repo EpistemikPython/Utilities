9303b64d049a452a4899cbd036d8a51c011482cc
epistemik <epistemik@gmail.com>
2021-05-08 12:19:46 -0400

    updates for python3.8.5

--- format_json.py
+++ format_json.py
@@ -3,24 +3,24 @@
 #
 # format_json.py -- format a JSON file with proper new lines and indents
 #
-# Copyright (c) 2019 Mark Sattolo <epistemik@gmail.com>
+# Copyright (c) 2019-21 Mark Sattolo <epistemik@gmail.com>
 #
 __author__ = 'Mark Sattolo'
 __author_email__ = 'epistemik@gmail.com'
-__python_version__ = 3.6
+__python_version__ = '3.6+'
 __created__ = '2019-02-25'
-__updated__ = '2019-08-30'
+__updated__ = '2021-05-08'
 
 import sys
 import json
 
 if len(sys.argv) < 3:
-	print("usage: python {} <JSON file> <indent>".format(sys.argv[0]))
-	sys.exit()
+    print(F"usage: python {sys.argv[0]} <JSON file> <indent>")
+    exit()
 
 # read the file
 with open(sys.argv[1]) as file:
-	content = json.loads( file.read() )
+    content = json.loads( file.read() )
 
 # dump output to nicely formatted JSON
 print( json.dumps( content, indent=int(sys.argv[2]) ) )
--- parse_args.py
+++ parse_args.py
@@ -3,28 +3,30 @@
 #
 # parse_args.py -- use ArgumentParser for command line variables
 #
-# Copyright (c) 2019 Mark Sattolo <epistemik@gmail.com>
+# Copyright (c) 2019-21 Mark Sattolo <epistemik@gmail.com>
 #
 __author__ = 'Mark Sattolo'
 __author_email__ = 'epistemik@gmail.com'
-__python_version__ = 3.6
+__python_version__ = '3.6+'
 __created__ = '2019-08-30'
-__updated__ = '2019-08-30'
+__updated__ = '2021-05-08'
 
 from argparse import ArgumentParser
 
-
 def test_args():
-	found_args = ArgumentParser(description="xxx")
-	# required arguments
-	found_args.add_argument('filename', action=store, help='xxx')
-	found_args.add_argument('runmode', action=store, help='prod OR test')
-	return found_args
-
+    found_args = ArgumentParser(description="xxx")
+    # required arguments
+    found_args.add_argument('filename', action='store', help='xxx')
+    found_args.add_argument('runmode', action='store', help='prod OR test')
+    return found_args
 
 def main():
-	args = test_args().parse_args()
+    args = test_args().parse_args()
+    fname = args.filename
+    mode = args.runmode.upper()
+    print(F"filename = {fname}; mode = {mode}")
 
-	fname = args.filename
 
-	mode = args.runmode.upper()
+if __name__ == "__main__":
+    main()
+    exit()
