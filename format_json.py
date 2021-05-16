4030aad446158b8f83653db3b79fda73ca56b15a
epistemik <epistemik@gmail.com>
2021-05-16 09:14:29 -0400

    update usage printout

--- format_json.py
+++ format_json.py
@@ -9,13 +9,13 @@ __author__ = 'Mark Sattolo'
 __author_email__ = 'epistemik@gmail.com'
 __python_version__ = '3.6+'
 __created__ = '2019-02-25'
-__updated__ = '2021-05-08'
+__updated__ = '2021-05-15'
 
 import sys
 import json
 
 if len(sys.argv) < 3:
-    print(F"usage: python {sys.argv[0]} <JSON file> <indent>")
+    print(F"usage: python3 {sys.argv[0]} <JSON file> <indent>")
     exit()
 
 # read the file
@@ -23,4 +23,4 @@ with open(sys.argv[1]) as file:
     content = json.loads( file.read() )
 
 # dump output to nicely formatted JSON
-print( json.dumps( content, indent=int(sys.argv[2]) ) )
+print( json.dumps( content, indent = int(sys.argv[2]) ) )
