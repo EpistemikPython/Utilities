583090242b0b2d165ea68431873bfd7c9e46e70b
epistemik <epistemik@gmail.com>
2024-09-07 17:20:30 -0400

    add exception handling; more arg checking; better error messages; &

--- format_json.py
+++ format_json.py
@@ -3,24 +3,58 @@
 #
 # format_json.py -- format a JSON file with proper new lines and indents
 #
-# Copyright (c) 2019-21 Mark Sattolo <epistemik@gmail.com>
+# Copyright (c) 2024 Mark Sattolo <epistemik@gmail.com>
 #
-__author__ = 'Mark Sattolo'
-__author_email__ = 'epistemik@gmail.com'
+__author__         = 'Mark Sattolo'
+__author_email__   = 'epistemik@gmail.com'
 __python_version__ = '3.6+'
 __created__ = '2019-02-25'
-__updated__ = '2021-05-15'
+__updated__ = '2024-09-06'
 
-import sys
+from sys import argv
 import json
+from mhsUtils import osp, save_to_json, get_base_filename, JSON_LABEL
 
-if len(sys.argv) < 3:
-    print(F"usage: python3 {sys.argv[0]} <JSON file> <indent>")
-    exit()
+DEFAULT_INDENT = 4
+MAX_INDENT = 16
+USAGE = f"Usage: python3 {argv[0]} 'path to {JSON_LABEL} file' <indent, DEFAULT = {DEFAULT_INDENT}>"
 
-# read the file
-with open(sys.argv[1]) as file:
-    content = json.loads( file.read() )
+def run():
+    print(f"argv = {argv}")
+    if len(argv) < 2:
+        print(USAGE)
+        raise Exception("NO file name.")
+    else:
+        if osp.isfile(argv[1]):
+            filename = argv[1]
+        else:
+            print(USAGE)
+            raise Exception("BAD file name.")
 
-# dump output to nicely formatted JSON
-print( json.dumps( content, indent = int(sys.argv[2]) ) )
+    jndent = DEFAULT_INDENT
+    if len(argv) >= 3:
+        ix = int(argv[2])
+        if 1 <= ix <= MAX_INDENT:
+            jndent = ix
+
+    # read the file
+    with open(filename) as jfile:
+        content = json.loads( jfile.read() )
+
+    # dump output to newly formatted JSON file
+    print(f"new {JSON_LABEL} file = {save_to_json(get_base_filename(filename), content, indt = jndent)}" )
+
+
+if __name__ == "__main__":
+    code = 0
+    try:
+        run()
+    except KeyboardInterrupt:
+        code = 13
+    except ValueError:
+        code = 27
+    except Exception as ex:
+        print( repr(ex) )
+        code = 66
+    print("Program finished.")
+    exit(code)
