4be839528fd7e6a5a9e53104c2a9fd4b5c4ffeed
epistemik <epistemik@gmail.com>
2024-11-09 10:55:42 -0500

    convert to my standard python file format for arg checking, main, run(), etc

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,7 +1,8 @@
 ##############################################################################################################################
 # coding=utf-8
 #
-# csv_to_json.py -- convert a CSV file to a JSON file with proper new lines and indents
+# csv_to_json.py
+#   -- convert a CSV file to a JSON file with proper new lines and indents
 #
 # Copyright (c) 2021 Mark Sattolo <epistemik@gmail.com>
 
@@ -9,37 +10,56 @@ __author__ = 'Mark Sattolo'
 __author_email__ = 'epistemik@gmail.com'
 __python_version__ = '3.6+'
 __created__ = '2021-08-26'
-__updated__ = '2021-08-26'
+__updated__ = '2024-11-09'
 
-import sys
+from sys import argv
 import csv
-import json
+from mhsUtils import osp, save_to_json, get_base_filename, JSON_LABEL
 
-def main_csv_to_json(csv_file:str, json_file:str, p_json_idt:str):
-    try:
-        json_indent = int(p_json_idt)
-    except ValueError:
-        print(F"bad json indent = {p_json_idt}; Using default value = 4.")
-        json_indent = 4
+DEFAULT_INDENT = 4
+MAX_INDENT = 16
+USAGE = f"Usage: python3 {get_base_filename(argv[0])} 'path to CSV file' <indent, DEFAULT = {DEFAULT_INDENT}>"
 
-    try:
-        # read the CSV file
-        with open(csv_file) as fc:
-            reader = csv.DictReader(fc)
-            rows = list(reader)
+def run():
+    print(f"argv = {argv}")
+    if len(argv) < 2:
+        print(USAGE)
+        raise Exception("NO file name.")
+    elif argv[1] == "-h":
+        print(USAGE)
+        return
+    elif osp.isfile(argv[1]):
+        csv_file = argv[1]
+    else:
+        print(USAGE)
+        raise Exception("BAD file name.")
 
-        # dump output to nicely formatted JSON
-        with open(json_file, 'x') as fj:
-            json.dump(rows, fj, indent = json_indent)
+    jndent = DEFAULT_INDENT
+    if len(argv) > 2:
+        ix = int(argv[2])
+        if 1 <= ix <= MAX_INDENT: jndent = ix
 
-    except FileNotFoundError as fnfe:
-        raise fnfe
+    # read the file
+    with open(csv_file) as fc:
+        reader = csv.DictReader(fc)
+        rows = list(reader)
+
+    # dump output to newly formatted JSON file
+    print(f"new {JSON_LABEL} file = {save_to_json(get_base_filename(csv_file), json_data = rows, indt = jndent)}" )
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print(F"usage: python3 {sys.argv[0]} <CSV file> <JSON file> <indent>")
-    else:
-        main_csv_to_json( sys.argv[1], sys.argv[2], sys.argv[3] )
-        print("Program finished.")
-    exit()
+    code = 0
+    try:
+        run()
+    except KeyboardInterrupt as mki:
+        print( repr(mki) )
+        code = 13
+    except ValueError as mve:
+        print( repr(mve) )
+        code = 27
+    except Exception as mex:
+        print( repr(mex) )
+        code = 66
+    print("Program finished.")
+    exit(code)
